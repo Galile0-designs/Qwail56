edfeb681c5cb2227d0c68fbd9c4ae204b8fa85a1
Galile0 <134774462+Galile0-designs@users.noreply.github.com>
2023-08-17 13:47:47 +0200

    Update main.py

--- qwail56 v1/firmware/main.py
+++ qwail56 v1/firmware/main.py
@@ -1,4 +1,5 @@
 # qwail56 - 60% ergo split keyboard https://github.com/Galile0-designs/qwail56
+
 import board
 
 from kmk.kmk_keyboard import KMKKeyboard
@@ -54,27 +55,27 @@ keyboard.keymap = [
     [
     KC.ESC,    KC.N1,  KC.N2,  KC.N3,   KC.N4,   KC.N5,    KC.NO,      KC.NO,   KC.N6,    KC.N7,   KC.N8,   KC.N9,  KC.N0,   KC.BSPC,
     KC.TAB,    KC.Q,   KC.W,   KC.E,    KC.R,    KC.T,     KC.NO,      KC.NO,   KC.Y,     KC.U,    KC.I,    KC.O,   KC.P,    KC.BSLS,
-    KC.LCTRL,  KC.A,   KC.S,   KC.D,    KC.F,    KC.G,     KC.NO,      KC.NO,   KC.H,     KC.J,    KC.K,    KC.L,   KC.QUOT, KC.ENT,
+    KC.LCTRL,  KC.A,   KC.S,   KC.D,    KC.F,    KC.G,     KC.NO,      KC.NO,   KC.H,     KC.J,    KC.K,    KC.L,   KC.SCLN, KC.QUOT,
     KC.LSHIFT, KC.Z,   KC.X,   KC.C,    KC.V,    KC.B,     KC.MPLY,    KC.MPLY, KC.N,     KC.M,    KC.COMM, KC.DOT, KC.SLSH, KC.RSFT,
-    KC.NO,     KC.NO,  KC.NO,  KC.LGUI, KC.LALT, KC.MO(1), KC.SPC,     KC.SPC,  KC.MO(2), KC.RALT, KC.RCTL, KC.NO,  KC.NO,   KC.NO,
+    KC.NO,     KC.NO,  KC.NO,  KC.LGUI, KC.LALT, KC.MO(1), KC.SPC,     KC.ENT,  KC.MO(2), KC.RALT, KC.RCTL, KC.NO,  KC.NO,   KC.NO,
     ],
 
     # function layer
     [
     KC.F1,     KC.F2,  KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.NO,      KC.NO,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,
     KC.CAPS,   KC.INS, KC.HOME, KC.PGUP, KC.NO,   KC.NO,   KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.PSCR,
-    KC.LCTRL,  KC.DEL, KC.END,  KC.PGDN, KC.NO,   KC.NO,   KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.UP,   KC.NO,   KC.ENT,
+    KC.LCTRL,  KC.DEL, KC.END,  KC.PGDN, KC.NO,   KC.NO,   KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.UP,   KC.NO,   KC.NO,
     KC.LSHIFT, KC.NO,  KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.MPLY,    KC.MPLY, KC.NO,   KC.NO,   KC.LEFT, KC.DOWN, KC.RGHT, KC.RSFT,
-    KC.NO,     KC.NO,  KC.NO,   KC.LGUI, KC.LALT, KC.TRNS, KC.SPC,     KC.SPC,  KC.TRNS, KC.RALT, KC.RCTL, KC.NO,   KC.NO,   KC.NO,
+    KC.NO,     KC.NO,  KC.NO,   KC.LGUI, KC.LALT, KC.TRNS, KC.SPC,     KC.ENT,  KC.TRNS, KC.RALT, KC.RCTL, KC.NO,   KC.NO,   KC.NO,
     ],
 
     # symbol layer
     [
     KC.ESC,    KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.GRV,  KC.PLUS, KC.BSPC,
-    KC.CAPS,   KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.LBRC, KC.RBRC, KC.BSLS,
+    KC.TAB,    KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.LBRC, KC.RBRC, KC.BSLS,
     KC.LCTRL,  KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.NO,      KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.SCLN, KC.ENT,
-    KC.LSHIFT, KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.MPLY,    KC.MPLY, KC.NO,   KC.NO,   KC.COMM, KC.DOT,  KC.MINUS,KC.RSFT,
-    KC.NO,     KC.NO,  KC.NO,  KC.LGUI, KC.LALT, KC.TRNS,  KC.SPC,     KC.SPC,  KC.TRNS, KC.RALT, KC.RCTL, KC.NO,   KC.NO,   KC.NO,
+    KC.LSHIFT, KC.NO,  KC.NO,  KC.NO,   KC.NO,   KC.NO,    KC.MPLY,    KC.MPLY, KC.NO,   KC.NO,   KC.COMM, KC.DOT,  KC.MINS, KC.RSFT,
+    KC.NO,     KC.NO,  KC.NO,  KC.LGUI, KC.LALT, KC.TRNS,  KC.SPC,     KC.ENT,  KC.TRNS, KC.RALT, KC.RCTL, KC.NO,   KC.NO,   KC.NO,
     ],
 ]
 
